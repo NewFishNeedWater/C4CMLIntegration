fe6197e0ff02d5b01816d514089f59c298a77f3c
289343554@qq.com <harry.zhang@sap.com>
2018-04-10 14:17:31 +0800

    Implementation error handling

--- src/main/java/com/sap/integration/web/machineLearning/C4CControllerForML.java
+++ src/main/java/com/sap/integration/web/machineLearning/C4CControllerForML.java
@@ -32,59 +32,71 @@ import java.util.Date;
 @RestController
 public class C4CControllerForML {
 
-    @Autowired
-    MLService mlService;
+	@Autowired
+	MLService mlService;
 
-    @Autowired
-    LogInfoService logInfoService;
+	@Autowired
+	LogInfoService logInfoService;
 
-    @Autowired
-    DeepLearningService deepLearningService;
+	@Autowired
+	DeepLearningService deepLearningService;
 
-    @Autowired
-    NaturalLanProcessService naturalLanProcessService;
+	@Autowired
+	NaturalLanProcessService naturalLanProcessService;
 
-    private static Logger logger = LoggerFactory
-            .getLogger(C4CControllerForML.class);
+	private static Logger logger = LoggerFactory
+			.getLogger(C4CControllerForML.class);
 
-    @ApiOperation(value = "AI API Wrapper", notes = "Get Whole AI Process Result", httpMethod = "POST")
-    @RequestMapping(value = "/getAIProcessResult", method = RequestMethod.POST)
-    public C4CUserActionResponse getAIProcessResult(
-            @RequestBody @ApiParam(name = "NLP API", value = "Get Whole AI Process Result", required = true) NaturalLanProcessRequest request) {
+	@ApiOperation(value = "AI API Wrapper", notes = "Get Whole AI Process Result", httpMethod = "POST")
+	@RequestMapping(value = "/getAIProcessResult", method = RequestMethod.POST)
+	public C4CUserActionResponse getAIProcessResult(
+			@RequestBody @ApiParam(name = "NLP API", value = "Get Whole AI Process Result", required = true) NaturalLanProcessRequest request) {
 
-        //get nature language response from recast API
-        NaturalLanProcessResponse nlpResponse = naturalLanProcessService.getNLResponse(request);
-        //convert the NLP response to deep learning API request
-        C4CUserActionVo deepLearnRequest = mlService.convertNLPToUserActionRequest(nlpResponse);
-        //get response from deep learning API
-        C4CUserActionResponse response = deepLearningService.generateResultEntry(deepLearnRequest);
+		try {
 
+			// get nature language response from recast API
+			NaturalLanProcessResponse nlpResponse = naturalLanProcessService
+					.getNLResponse(request);
+			// convert the NLP response to deep learning API request
+			C4CUserActionVo deepLearnRequest = mlService
+					.convertNLPToUserActionRequest(nlpResponse);
+			// get response from deep learning API
+			C4CUserActionResponse response = deepLearningService
+					.generateResultEntry(deepLearnRequest);
 
-        //save log
-        logger.warn("getRelatedAction--Request from external:"
-                + JSONObject.fromObject(request).toString());
+			// save log
+			logger.warn("getRelatedAction--Request from external:"
+					+ JSONObject.fromObject(request).toString());
 
-        LogInfo log = new LogInfo();
+			LogInfo log = new LogInfo();
 
-        log.setApiName("getAIProcessResult");
+			log.setApiName("getAIProcessResult");
 
-        log.setApiType("IN");
+			log.setApiType("IN");
 
-        log.setStartTime(new Date());
+			log.setStartTime(new Date());
 
-        log.setRequest(JSONObject.fromObject(request).toString());
+			log.setRequest(JSONObject.fromObject(request).toString());
 
-        logger.warn("getRelatedAction--Response to external:"
-                + JSONObject.fromObject(response).toString());
+			logger.warn("getRelatedAction--Response to external:"
+					+ JSONObject.fromObject(response).toString());
 
-        log.setEndTime(new Date());
+			log.setEndTime(new Date());
 
-        log.setResponse(JSONObject.fromObject(response).toString());
+			log.setResponse(JSONObject.fromObject(response).toString());
 
-        logInfoService.saveLog(log);
+			logInfoService.saveLog(log);
 
-        return response;
-    }
+			return response;
 
+		} catch (Exception ex) {
+			
+			C4CUserActionResponse response = new C4CUserActionResponse();
+			
+			return response;
+
+		}
+
+	}
 
 }
